57187451b7685ed7ac3c06dac19c7caac49fbe0a
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2023-12-13 20:59:25 -0600

    Error handler middleware added!

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,14 @@ import express from "express";
 import apiRoutes from "./api";
 import bodyParser from "body-parser";
 
+import errorHandler from "./middlewares/errorHandler";
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api", apiRoutes);
+
+app.use(errorHandler);
 
 export default app;
